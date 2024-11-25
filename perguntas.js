2eea99912c695314e871e30fa58f797e5a234384
Professora-1 <karinacoelho@prof.educacao.sp.gov.br>
2024-11-25 13:16:40 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,11 @@
 criaCartao(
     'Programação',
+    'O que é Javascript?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
     'O que é Python?',
     'O Python é uma linguagem de programação'
 )
